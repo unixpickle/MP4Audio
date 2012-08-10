43c1453af00a4b23809e954e15cc2cb14d96847b
Alex Nichol <alexnichol@comcast.net>
2012-08-10 17:24:49 -0400

    Initial Commit

---
+++ MP4Audio/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MP4Audio
+//
+//  Created by Alex Nichol on 8/10/12.
+//  Copyright (c) 2012 Alex Nichol. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
